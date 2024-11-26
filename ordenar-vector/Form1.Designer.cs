23bfdc8ee6d028e574062d2203f8a8586456a1f4
devjpunk <91user101@protonmail.com>
2024-11-25 18:52:48 -0600

    oredenamiento de vector

---
+++ ordenar-vector/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ordenar_vector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtVector = new System.Windows.Forms.TextBox();
+            this.lblOrden = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNum.Location = new System.Drawing.Point(350, 51);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 26);
+            this.txtNum.TabIndex = 0;
+            this.txtNum.Click += new System.EventHandler(this.txtNum_Click);
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
+            // 
+            // txtVector
+            // 
+            this.txtVector.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtVector.Location = new System.Drawing.Point(266, 154);
+            this.txtVector.Name = "txtVector";
+            this.txtVector.Size = new System.Drawing.Size(100, 26);
+            this.txtVector.TabIndex = 2;
+            // 
+            // lblOrden
+            // 
+            this.lblOrden.AutoSize = true;
+            this.lblOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblOrden.Location = new System.Drawing.Point(282, 80);
+            this.lblOrden.Name = "lblOrden";
+            this.lblOrden.Size = new System.Drawing.Size(230, 20);
+            this.lblOrden.TabIndex = 1;
+            this.lblOrden.Text = "Escribe un numero y presionalo";
+            this.lblOrden.Click += new System.EventHandler(this.lblOrden_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtVector);
+            this.Controls.Add(this.lblOrden);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtVector;
+        private System.Windows.Forms.Label lblOrden;
+    }
+}
+
---
+++ ordenar-vector/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordenar_vector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtVector.Multiline = true;
+            txtVector.ReadOnly = true;
+        }
+        Random random = new Random();
+
+        //variables para el tamano del texbox
+        float factorAncho = 1.6f;
+        float factorAlto = 1.2f;
+
+        private async void txtNum_Click(object sender, EventArgs e)
+        {
+            txtVector.Clear();
+            if (int.TryParse(txtNum.Text, out int number))
+            {
+                int[] vector = new int[number];
+
+                //Define tamano celdas
+                int altoCelda = 10;
+                int anchoCelda = 30;
+
+                //Modifica el tamano del texbox
+                txtVector.Width = (int)((number * anchoCelda) * factorAncho);
+                txtVector.Height = (int)((number * altoCelda) * factorAlto);
+
+                for (int i = 0; i < number; i++)
+                {
+                    vector[i] = random.Next(50);
+                }
+
+                //Imprimir arrgelo original
+                foreach(int i in vector)
+                {
+                    txtVector.AppendText( $"|  {i}  ");
+                    await Task.Delay(500);
+                }
+                txtVector.Text += "|";
+                await Task.Delay(2000);
+                txtVector.Clear();
+
+                //Imprimir arreglo ordenado
+                var oredenada = vector.OrderBy(n => n).ToArray();
+
+                foreach (var i in oredenada)
+                {
+                    txtVector.AppendText($"|  {i}  ");
+                    await Task.Delay(500);
+                }
+                txtVector.Text += "|";
+            }
+            else if (txtNum.Text == "")
+            { }
+            else
+            {
+                MessageBox.Show("Ingresa un numero entero");
+            }
+        }
+
+        public void txtNum_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblOrden_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
